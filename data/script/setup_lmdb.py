a644afcba79a14232608a3a5433b97e070869c53
Peter Ruch <gfrogat@gmail.com>
2018-02-18 13:48:14 +0100

    added first api draft

---
+++ data/script/setup_lmdb.py
@@ -0,0 +1,84 @@
+from __future__ import print_function
+
+import lmdb
+import numpy as np
+import os
+import os.path
+import pickle
+
+
+def download_cifar10(root):
+  from six.moves import urllib
+  import tarfile
+
+  url = 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
+  filename = 'cifar-10-python.tar.gz'
+
+  root = os.path.expanduser(root)
+  fpath = os.path.join(root, filename)
+
+  if not os.path.exists(root):
+    os.makedirs(root)
+
+  if os.path.isfile(fpath):
+    print('Cifar10 already downloaded')
+  else:
+    print('Downloading CIFAR10 from ' + url + ' to ' + fpath)
+    urllib.request.urlretrieve(url, fpath)
+
+  cwd = os.getcwd()
+  tar = tarfile.open(fpath, "r:gz")
+  os.chdir(root)
+  tar.extractall()
+  tar.close()
+  os.chdir(cwd)
+
+
+def load_cifar_batch(filename, root):
+  fpath = os.path.join(root, filename)
+  with open(fpath, 'rb') as cifar_batch_pickle:
+      # unpickle files and read in as dictionary object
+      cifar_batch = pickle.load(cifar_batch_pickle)
+  return cifar_batch
+
+
+def setup_lmdb(root):
+  batch_files = [
+    'cifar-10-batches-py/data_batch_1',
+    'cifar-10-batches-py/data_batch_2',
+    'cifar-10-batches-py/data_batch_3',
+    'cifar-10-batches-py/data_batch_4',
+    'cifar-10-batches-py/data_batch_5']
+
+  root = os.path.expanduser(root)
+  dbname = 'lmdb_cifar10_train'
+  dbpath = os.path.join(root, dbname)
+
+  map_size = 10 * 30720000
+  env = lmdb.open(dbpath, map_size=map_size)
+
+  img_id = 0
+
+  for filename in batch_files:
+    batch_data = load_cifar_batch(filename, root)
+
+    # load and reshape images [N, C, H, W] ... image, channel, height, width
+    images = batch_data[b'data'].reshape([-1, 3, 32, 32])
+    # reshape to [N, H, W, C] ... image, height, width, channel
+    images = images.transpose(0, 2, 3, 1)
+    # load labels and filenames for index file
+
+    with env.begin(write=True) as txn:
+      for image in images:
+        str_id = '{:08}'.format(img_id)
+        data = image.tostring()
+        txn.put(str_id, data)
+        img_id += 1
+
+
+if __name__ == "__main__":
+  cwd = os.path.dirname(os.path.realpath(__file__))
+  root = os.path.join(cwd, '../images')
+
+  download_cifar10(root)
+  setup_lmdb(root)
--- malevo/api.py
+++ malevo/api.py
@@ -4,9 +4,91 @@
 # Licensed under the new BSD license, available at http://caleydo.org/license
 ###############################################################################
 
+from __future__ import print_function
+
+from PIL import Image
+import lmdb
+import numpy as np
+import os
+import os.path
+import math
+import sqlite3
 from phovea_server.ns import Namespace
 app = Namespace(__name__)
+from flask import request
+from .util import IntListConverter, serve_pil_image
+app.url_map.converters['integerList'] = IntListConverter
+
+os.environ['SQLITE_TMPDIR'] = '/tmp'
+
+cwd = os.path.dirname(os.path.realpath(__file__))
+root = os.path.join(cwd, '../data/images')
+
+
+@app.route("/confmat/cell/imageIds", methods=['GET'])
+def _get_image_ids():
+    run_id = request.args.get('runId', '')
+    epoch_id = request.args.get('epochId', 0)
+    ground_truth_id = request.args.get('groundTruthClassId', 0)
+    predicted_id = request.args.get('predictedId', 0)
+    num_count = request.args.get('numCount', 100)
+
+    query = (epoch_id, ground_truth_id, predicted_id)
+
+    conn = sqlite3.connect('../data/example.db')
+    c = conn.cursor()
+    c.execute("SELECT img_id FROM logs WHERE epoch_id = ? AND ground_truth = ? AND predicted = ?", query)
+    img_ids_tuples = c.fetchall()[:num_count]
+
+    conn.close()
+
+    return [img_id[0] for img_id in img_ids_tuples]
+
+
+#@app.route('/images/imageSprite/<int:runId>/<integerList:imageIds>', methods=['GET'])
+#def _get_image_sprite(runId, imageIds):
+    #run_id = runId
+    #img_ids = imageIds
+@app.route('/images/imageSprite', methods=['GET'])
+def _get_image_sprite():
+    run_id = request.args.get('runId', '')
+    img_ids = request.args.get('imageIds', [])
+    img_ids = map(int, img_ids.split(','))
+
+    n_imgs = len(img_ids)
+
+    if len(img_ids) == 0:
+      return 'error'
+
+    dbname = 'lmdb_cifar10_train'
+    dbpath = os.path.join(root, dbname)
+    env = lmdb.open(dbpath)
+
+    n_w = 10
+    n_h = int(math.ceil(n_imgs / float(n_w)))
+
+    width = n_w * 32
+    height = n_h * 32
+
+    master = Image.new(
+        mode='RGB',
+        size=(width, height),
+        color=(0,0,0))  # fully transparent
+
+    k = 0
+    with env.begin() as txn:
+        for i in range(0, n_h):
+            for j in range(0, n_w):
+                if k == n_imgs:
+                  break
+                str_id = '{:08}'.format(img_ids[k])
+                img = Image.fromarray(
+                    np.frombuffer(txn.get(str_id), dtype=np.uint8).reshape((32, 32, 3)))
+                master.paste(img, (j*32, i*32))
+                k += 1
+
+    return serve_pil_image(master)
 
 
 def create():
-  return app
+    return app
