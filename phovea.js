fd9511623b13d709d3f287a1b35df7e6c10fb8ab
Martin Ennemoser <martin.ennemoser@jku.at>
2018-07-03 10:40:15 +0200

    Renamed DetailChart to DetailChartTab

--- phovea.js
+++ phovea.js
@@ -32,10 +32,10 @@ module.exports = function (registry) {
     'name': 'ToolbarView'
   });
 
-  registry.push('malevoView', 'DetailChart', function () {
+  registry.push('malevoView', 'DetailChartTab', function () {
     return System.import('./src/detail_view/DetailChart');
   }, {
-    'name': 'DetailChart',
+    'name': 'DetailChartTab',
     'isDetailWindow': true,
     'order': 10
   });
