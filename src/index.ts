a405e1f3ec4eb471aa7f6589ffb8357d6bb5fce5
Holger Stitz <holger.stitz@jku.at>
2018-09-13 14:59:37 +0200

    Point report bug link to github issues

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,21 @@ import 'file-loader?name=404.html-loader!./404.html';
 import 'file-loader?name=robots.txt!./robots.txt';
 import 'phovea_ui/src/_bootstrap';
 import './style.scss';
-import {create as createApp} from './app';
-import {create as createHeader, AppHeaderLink} from 'phovea_ui/src/header';
-import {Language} from './language';
+import { create as createApp } from './app';
+import { create as createHeader, AppHeaderLink } from 'phovea_ui/src/header';
+import { Language } from './language';
 
 createHeader(
   <HTMLElement>document.querySelector('#caleydoHeader'),
   { appLink: new AppHeaderLink(Language.APP_NAME) }
 );
+
+// modify the report bug link that it links directly to github issues
+const reportBugLink: HTMLElement = (<HTMLElement>document.querySelector('[data-header="bugLink"] > a'));
+reportBugLink.setAttribute('href', 'https://github.com/Caleydo/malevo/issues/new');
+reportBugLink.setAttribute('target', '_blank');
+reportBugLink.removeAttribute('data-toggle');
+reportBugLink.removeAttribute('data-target');
 
 const parent = document.querySelector('#app');
 createApp(parent).init();
